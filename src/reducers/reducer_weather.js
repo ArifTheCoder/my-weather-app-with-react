0247b8ff1ab300dac18371c22d45e5439ffaa4b1
Arif Hossain <arifthecoder@gmail.com>
2018-09-03 16:53:17 +0300

    action validation has been done

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,11 @@
 import {FETCH_WEATHER} from '../actions/index';
 
 export default function(state = [], action){
+
+    if(action.error){
+        alert('Wrong city name! Please enter a valid city name of Finland!');
+        return state;
+    }
     switch(action.type){
         case FETCH_WEATHER:
         return [action.payload.data, ...state];   // returns a completely new array with new values
